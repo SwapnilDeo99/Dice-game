c6491463bb2b9e8a8ee9f054339ef1bc40d08a5d
Swapnil Deo <99477450+SwapnilDeo99@users.noreply.github.com>
2024-01-23 23:43:55 +0530

    index.js

--- index.js
+++ index.js
@@ -5,64 +5,64 @@ switch (d1) {
   case 1:
     document
       .getElementById("d1")
-      .setAttribute("src", "/Dice game/img/dice1.png");
+      .setAttribute("src", "/img/dice1.png");
     break;
   case 2:
     document
       .getElementById("d1")
-      .setAttribute("src", "/Dice game/img/dice2.png");
+      .setAttribute("src", "/img/dice2.png");
     break;
   case 3:
     document
       .getElementById("d1")
-      .setAttribute("src", "/Dice game/img/dice3.png");
+      .setAttribute("src", "/img/dice3.png");
     break;
   case 4:
     document
       .getElementById("d1")
-      .setAttribute("src", "/Dice game/img/dice4.png");
+      .setAttribute("src", "/img/dice4.png");
     break;
   case 5:
     document
       .getElementById("d1")
-      .setAttribute("src", "/Dice game/img/dice5.png");
+      .setAttribute("src", "/img/dice5.png");
     break;
   case 6:
     document
       .getElementById("d1")
-      .setAttribute("src", "/Dice game/img/dice6.png");
+      .setAttribute("src", "/img/dice6.png");
     break;
 }
 switch (d2) {
   case 1:
     document
       .getElementById("d2")
-      .setAttribute("src", "/Dice game/img/dice1.png");
+      .setAttribute("src", "/img/dice1.png");
     break;
   case 2:
     document
       .getElementById("d2")
-      .setAttribute("src", "/Dice game/img/dice2.png");
+      .setAttribute("src", "/img/dice2.png");
     break;
   case 3:
     document
       .getElementById("d2")
-      .setAttribute("src", "/Dice game/img/dice3.png");
+      .setAttribute("src", "/img/dice3.png");
     break;
   case 4:
     document
       .getElementById("d2")
-      .setAttribute("src", "/Dice game/img/dice4.png");
+      .setAttribute("src", "/img/dice4.png");
     break;
   case 5:
     document
       .getElementById("d2")
-      .setAttribute("src", "/Dice game/img/dice5.png");
+      .setAttribute("src", "/img/dice5.png");
     break;
   case 6:
     document
       .getElementById("d2")
-      .setAttribute("src", "/Dice game/img/dice6.png");
+      .setAttribute("src", "/img/dice6.png");
     break;
 }
 if(d1 > d2){
